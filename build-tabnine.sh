dc300b8bfed8c016db38667717ea86df669d8a17
Juxuny Wu <juxuny@163.com>
2023-10-11 19:28:27 +0800

    fix

--- build-tabnine.sh
+++ build-tabnine.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 set -e
-if [ -f dl.sh ]; then ./dl.sh; fi
+if [ -f dl.sh ]; then sh dl.sh; fi
 REPOSITORY=registry.cn-shenzhen.aliyuncs.com/juxuny-public/vim
 VERSION=go1.20-v1.7.1-$(uname | awk '{print tolower($0)}')
 docker build -t ${REPOSITORY}:${VERSION} -f tabnine.dockerfile . --no-cache
