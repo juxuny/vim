e5814caa279edf62102015cfd89743f772cbb2b6
Juxuny Wu <juxuny@gmail.com>
2023-11-30 03:31:24 +0800

    fix

--- install-gcc.sh
+++ install-gcc.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
-set -e
+set -ex
 src_dir=/open-source
+
+
 function download_gcc_source() {
 	version="$1"
 	if [ ! -d "${src_dir}" ]; then mkdir ${src_dir}; fi
@@ -19,14 +21,14 @@ function compile_and_install() {
 	cd - && rm -rf gcc-${version}
 }
 
-download_gcc_source 10.5.0
-download_gcc_source 11.4.0
-download_gcc_source 12.3.0
-download_gcc_source 13.2.0
-
 apt-get update && apt-get install -y libmpc-dev libgmp-dev libmpfr-dev gcc-multilib
 
-compile_and_install 10.5.0
-compile_and_install 11.4.0
-compile_and_install 12.3.0
-compile_and_install 13.2.0
+version_list="10.4.0 11.3.0 12.2.0"
+for version in ${version_list}; do
+	echo "compiling version: ${version}"
+	download_gcc_source ${version}
+done
+for version in ${version_list}; do
+	echo "compiling version: ${version}"
+	compile_and_install ${version}
+done
