3c2d7a654f9b5f37cce5b0b552a0c881dbe03e98
Juxuny Wu <juxuny@gmail.com>
2024-04-01 00:50:09 +0800

    upgrade node

--- dl.sh
+++ dl.sh
@@ -13,5 +13,7 @@ download_link() {
 #download_link https://go.dev/dl/go1.20.8.linux-amd64.tar.gz
 download_link https://go.dev/dl/go1.21.8.linux-amd64.tar.gz
 #download_link https://nodejs.org/download/release/v16.20.0/node-v16.20.0-linux-x64.tar.gz
-download_link https://nodejs.org/download/release/v20.11.0/node-v20.11.0-linux-x64.tar.gz
+#download_link https://nodejs.org/download/release/v20.11.0/node-v20.11.0-linux-x64.tar.gz
+#download_link https://nodejs.org/download/release/v20.12.0/node-v20.12.0-linux-x64.tar.gz
+download_link https://nodejs.org/download/release/v21.7.0/node-v21.7.0-linux-x64.tar.gz
 download_link https://github.com/clangd/clangd/releases/download/16.0.2/clangd-linux-16.0.2.zip
